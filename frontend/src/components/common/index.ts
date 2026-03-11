ba41b509c24d26e6e8819cf11ebc6fd9740bb4b7
Sathvika788 <pappulasathvikareddy@gmail.com>
2026-03-11 14:42:33 +0530

    Delete frontend directory

--- frontend/src/components/common/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as Card } from './Card';
-export { default as Button } from './Button';
-export { default as Badge } from './Badge';
-export { default as Input } from './Input';
-export { default as StatCard } from './StatCard';
-export { default as Logo } from './Logo';
